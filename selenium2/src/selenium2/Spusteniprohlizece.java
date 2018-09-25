efa7e15a9c4a302a5769bf65c13d5dfd2a2c654b
Agata P <agyagy@gmail.com>
2018-09-25 15:41:53 +0200

    nastartovani chromu

--- selenium2/src/selenium2/Spusteniprohlizece.java
+++ selenium2/src/selenium2/Spusteniprohlizece.java
@@ -1,10 +1,29 @@
 package selenium2;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+import java.util.concurrent.TimeUnit;
+
+
 public class Spusteniprohlizece {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//	 System.out.println(System.getProperty("webdriver.chrome.driver"));	
+	 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tomas Bilka\\Desktop\\selenium_podklady\\chromedriver_win32\\chromedriver.exe");
+	 driver = createDriver();  	// slo by i bez prirazeni, protoze je to staticka var i staticka metoda
+	 driver.get("http://www.google.com");//////////**
+	 
 	}
+	
+	public static WebDriver driver;
+	
+	public static WebDriver createDriver() {	
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	return driver; 
+	}
+
+	
 
 }
